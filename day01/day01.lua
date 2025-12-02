992b0ae68f1c4e5704821ce9de6c78b0866596e3
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2025-12-01 22:37:41 -0600

    solve the part 2.

--- day01/day01.lua
+++ day01/day01.lua
@@ -1,7 +1,6 @@
 -- Advent of Code 2025 - Day 1
--- https://adventofcode.com/2025/day/1
+-- https://adventofchde.com/2025/day/1
 
--- Read input file
 local function read_input(filename)
     local lines = {}
     local file = io.open(filename, "r")
@@ -11,23 +10,36 @@ local function read_input(filename)
 
     for line in file:lines() do
         table.insert(lines, line)
-   end
+    end
     file:close()
     return lines
 end
+
+local function rotate(cur, dir)
+  -- Validate direction early
+  if dir ~= "L" and dir ~= "R" then
+    error("invalid direction: " .. tostring(dir))
+  end
+
+  if dir == "L" then
+    return (cur == 0) and 99 or cur - 1
+  else  -- dir == "R"
+    return (cur == 99) and 0 or cur + 1
+  end
+end
+
 
 -- Part 1
 local function part1(input)
     local ret = 0
     local cur = 50
-    for i, line in pairs(input) do
+    for _, line in pairs(input) do
       local rot = tonumber(string.sub(line, 2))
-      if string.find(line, "L") ~= nil then
-        cur = (cur + rot) % 100
-      else
-        cur = (cur + 100 - rot) % 100
-      end
+      local dir = string.sub(line, 1, 1)
 
+      for _ = 1,rot do
+        cur = rotate(cur, dir)
+      end
       if cur == 0 then
         ret = ret + 1
       end
@@ -37,8 +49,23 @@ end
 
 -- Part 2
 local function part2(input)
-    -- TODO: Implement part 2
-    return 0
+    -- Be careful: if the dial were pointing at 50, 
+    -- a single rotation like R1000 would cause the dial to point 
+    -- at 0 ten times before returning back to 50!
+    local ret = 0
+    local cur = 50
+    for _, line in pairs(input) do
+      local rot = tonumber(string.sub(line, 2))
+      local dir = string.sub(line, 1, 1)
+
+      for _ = 1,rot do
+        cur = rotate(cur, dir)
+        if cur == 0 then
+          ret = ret + 1
+        end
+      end
+    end
+    return ret
 end
 
 -- Main execution
