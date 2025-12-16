987e22087e31547f79c0896aa36648ded63c674e
Hyun-Hwan Jeong <hyun-hwan.jeong@bcm.edu>
2025-12-16 01:15:37 -0600

    solve part2 - with dfs (memoization) and inclusion/exclusion.

--- day11/day11.lua
+++ day11/day11.lua
@@ -11,17 +11,45 @@ local function read_input(filename)
   return lines
 end
 
-local function split_wspace(line)
-  local t = {}
-  for tok in line:gmatch("%S+") do
-    t[#t + 1] = tok:gsub(":", "")
-  end
+local function split_wspace(line) local t = {} for tok in line:gmatch("%S+") do t[#t + 1] = tok:gsub(":", "") end
   return t
 end
 
+
+local function count_path(G, start, goal)
+  local memo = {}
+
+  local function dfs(node)
+    -- Base Case: We reached the goal
+    if node == goal then
+      return 1
+    end
+
+    -- Check Cache: Have we already counted paths from this node?
+    if memo[node] then
+      return memo[node]
+    end
+
+    local total_paths = 0
+    local edges = G[node]
+
+    if edges then
+      for _, neighbor in pairs(edges) do
+        total_paths = total_paths + dfs(neighbor)
+      end
+    end
+
+    -- Store result in cache before returning
+    memo[node] = total_paths
+    return total_paths
+  end
+
+  return dfs(start)
+end
+
+
 -- Part 1
 local function part1(input)
-  -- TODO: Implement part 1
   local G = {}
   for i=1,#input do
     local l = split_wspace(input[i])
@@ -35,53 +63,61 @@ local function part1(input)
       local to = l[j]
       if G[to] == nil then
         G[to] = {}
-      end
-      table.insert(G[fr], to)
+      end table.insert(G[fr], to)
     end
   end
 
-  local ans = {}
-  local visited = {}
-  local cur = {"you"}
-
-  ans["you"] = 1
-
-  while #cur > 0 do
-    local next = {}
-    for _,node in pairs(cur) do
-      if visited[node] ~= true then
-        visited[node] = true
-        local E = G[node]
-        for _,to in pairs(E) do
-          if visited[to] ~= nil then
-            goto continue
-          end
-          table.insert(next, to)
-          if ans[to] == nil then
-            ans[to] = 0
-          end
-          ans[to] = ans[to] + ans[node]
-          ::continue::
-        end
-      end
-      cur = next
-    end
-  end
-
-  return ans["out"]
+  return count_path(G, "you", "out")
 end
 
--- Part 2
+
+local function build_graph(input, blockers)
+  local G = {}
+  for i=1,#input do
+    local l = split_wspace(input[i])
+
+    local fr = l[1]
+
+    if blockers[fr] ~= nil then
+      goto skip
+    end
+    if G[fr] == nil then
+      G[fr] = {}
+    end
+
+    for j=2,#l do
+      local to = l[j]
+      if blockers[to] == nil and G[to] == nil then
+        G[to] = {}
+      end
+      if blockers[to] == nil then
+        table.insert(G[fr], to)
+      end
+    end
+    ::skip::
+  end
+
+  return G
+end
+
 local function part2(input)
-  -- TODO: Implement part 2
-  return 0
-end
+  local G = build_graph(input, {})
+  local G_not_fft = build_graph(input, {fft = true})
+  local G_not_dac = build_graph(input, {dac = true})
+  local G_not_fft_dac = build_graph(input, {fft = true, dac = true})
 
+  local c1 = count_path(G, "svr", "out")
+  local c2 = count_path(G_not_fft, "svr", "out")
+  local c3 = count_path(G_not_dac, "svr", "out")
+  local c4 = count_path(G_not_fft_dac, "svr", "out")
+
+  print(c1, c2 , c3, c4)
+  return c1 - c2 - c3 + c4
+end
 -- Main execution
 local function main()
   local filename = arg[1] or "input.txt"
   local input = read_input(filename)
-  
   print("Part 1: " .. part1(input))
   print("Part 2: " .. part2(input))
 end
